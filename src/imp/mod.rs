302854edb51354a6d7f657da29331be24f5c4ff6
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2023-10-08 17:20:47 +0200

    refactor: use cfg-if for implementation module
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- src/imp/mod.rs
+++ src/imp/mod.rs
@@ -1,24 +1,18 @@
-#[cfg(not(any(target_os = "none", unix)))]
-mod unsupported;
-#[cfg(not(any(target_os = "none", unix)))]
-pub use unsupported::*;
-
-#[cfg(all(target_os = "none", target_arch = "aarch64"))]
-mod aarch64;
-#[cfg(all(target_os = "none", target_arch = "aarch64"))]
-pub use aarch64::*;
-
-#[cfg(all(target_os = "none", target_arch = "riscv64"))]
-mod riscv64;
-#[cfg(all(target_os = "none", target_arch = "riscv64"))]
-pub use riscv64::*;
-
-#[cfg(all(target_os = "none", target_arch = "x86_64"))]
-mod x86_64;
-#[cfg(all(target_os = "none", target_arch = "x86_64"))]
-pub use x86_64::*;
-
-#[cfg(unix)]
-mod unix;
-#[cfg(unix)]
-pub use unix::*;
+cfg_if::cfg_if! {
+    if #[cfg(unix)] {
+        mod unix;
+        pub use self::unix::*;
+    } else if #[cfg(all(target_os = "none", target_arch = "aarch64"))] {
+        mod aarch64;
+        pub use self::aarch64::*;
+    } else if #[cfg(all(target_os = "none", target_arch = "riscv64"))] {
+        mod riscv64;
+        pub use self::riscv64::*;
+    } else if #[cfg(all(target_os = "none", target_arch = "x86_64"))] {
+        mod x86_64;
+        pub use self::x86_64::*;
+    } else {
+        mod unsupported;
+        pub use self::unsupported::*;
+    }
+}
